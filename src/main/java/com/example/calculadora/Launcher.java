e0a8baeb8de4fcc8d6b6a3ca97cb571d237bef45
Renan <renan251101@edu.unipar.br>
2025-11-17 13:17:25 -0300

    Initial commit

---
+++ src/main/java/com/example/calculadora/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.calculadora;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
